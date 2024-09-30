f675e610d810d843245272cd9234232967967d20
dextmo <m6649964@gmail.com>
2024-09-30 11:06:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "21727017"))
 API_HASH = os.environ.get("API_HASH", "8c9ff17d8d1f6fbe228e5133b011fa99")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://alihaudff:111@cluster0.wqfkesd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://dexmoer:dex23@dex.nm2sl.mongodb.net/?retryWrites=true&w=majority&appName=dex")
